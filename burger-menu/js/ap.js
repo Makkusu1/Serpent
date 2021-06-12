136ee49cda92018f141821d610e6745a5eb51d8e
Makkusu1 <82879778+Makkusu1@users.noreply.github.com>
2021-06-12 21:35:16 +0200

    Delete burger-menu directory

--- burger-menu/js/ap.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('mousemove', (e)=> {
-    const raoul = document.createElement('div')
-    raoul.classList.add('rond')
-    document.querySelector('body').append(raoul)
-    raoul.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px;`)
-    setTimeout(() => {
-        raoul.remove()
-    }, 1000);
-})
