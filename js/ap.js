785f499d4617d22cb6c3472fa18f77e60877b9cf
Makkusu1 <82879778+Makkusu1@users.noreply.github.com>
2021-06-13 01:39:20 +0200

    Update ap.js

--- js/ap.js
+++ js/ap.js
@@ -8,11 +8,12 @@ document.addEventListener('mousemove', (e)=> {
     }, 1000);
 })
 
+
 document.addEventListener('touchmove', (e)=> {
     const raoul = document.createElement('div')
     raoul.classList.add('rond')
     document.querySelector('body').append(raoul)
-    raoul.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px;`)
+    raoul.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px;`)
     setTimeout(() => {
         raoul.remove()
     }, 1000);
