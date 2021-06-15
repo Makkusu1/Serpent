b8e6e4b4235e82fea4d7139b5f29a62072e83b90
Makkusu1 <82879778+Makkusu1@users.noreply.github.com>
2021-06-15 13:40:30 +0200

    Add files via upload

---
+++ js/ap.js
@@ -0,0 +1,118 @@
+
+const TAILLE = document.querySelector('#taille')
+const COULEUR = document.querySelector('#couleur')
+const COULEUR_PREVIEW = document.querySelector('.colorbutton')
+const BURGER = document.querySelector('.burger-menu')
+const PARAM = document.querySelector('.settings')
+const TRAIT = document.querySelector('#trait')
+
+document.addEventListener('mousemove', (e)=> {
+    const ROND = document.createElement('div')
+    ROND.classList.add('rond')
+    document.querySelector('body').append(ROND)
+    if (COULEUR.value == 'Multicolor')
+    {
+        
+        COULEUR_PREVIEW.style.background = ''
+        COULEUR_PREVIEW.style.backgroundImage = 'url("img/multicolor.jpg")'
+        if(TRAIT.value == 'solid')
+        {
+            ROND.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px; animation : multicolorP ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'solid 6px')
+        {
+            ROND.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px; animation : multicolorPG ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'solid 1.5px')
+        {
+            ROND.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px; animation : multicolorPF ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'dashed')
+        {
+            ROND.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px; animation : multicolorD ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'dashed 6px')
+        {
+            ROND.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px; animation : multicolorDG ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'dashed 1.5px')
+        {
+            ROND.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px; animation : multicolorDF ${TAILLE.value}0ms linear forwards;`)
+
+        }
+
+    }
+    else {
+        ROND.setAttribute('style', `top : ${e.pageY}px; left : ${e.pageX}px; animation : ret ${TAILLE.value}0ms linear forwards; border: ${TRAIT.value} ${COULEUR.value};`)
+        COULEUR_PREVIEW.style.background = COULEUR.value
+    }
+      setTimeout(() => {
+        ROND.remove()
+    }, `${TAILLE.value}0`);
+})
+
+document.addEventListener('touchmove',(e)=> {
+    const ROND = document.createElement('div')
+    ROND.classList.add('rond')
+    document.querySelector('body').append(ROND)
+    if (COULEUR.value == 'Multicolor')
+    {
+
+        COULEUR_PREVIEW.style.background = ''
+        COULEUR_PREVIEW.style.backgroundImage = 'url("img/multicolor.jpg")'
+        if(TRAIT.value == 'solid')
+        {
+            ROND.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px; animation : multicolorP ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'solid 6px')
+        {
+            ROND.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px; animation : multicolorPG ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'solid 1.5px')
+        {
+            ROND.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px; animation : multicolorPF ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'dashed')
+        {
+            ROND.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px; animation : multicolorD ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'dashed 6px')
+        {
+            ROND.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px; animation : multicolorDG ${TAILLE.value}0ms linear forwards;`)
+
+        }
+        else if (TRAIT.value == 'dashed 1.5px')
+        {
+            ROND.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px; animation : multicolorDF ${TAILLE.value}0ms linear forwards;`)
+
+        }
+
+
+    }
+    else {
+        ROND.setAttribute('style', `top : ${e.targetTouches[0].pageY}px; left : ${e.targetTouches[0].pageX}px; animation : ret ${TAILLE.value}0ms linear forwards; border: ${TRAIT.value} ${COULEUR.value};`)
+        COULEUR_PREVIEW.style.background = COULEUR.value
+    }
+      setTimeout(() => {
+        ROND.remove()
+    }, `${TAILLE.value}0`);
+
+})
+
+BURGER.addEventListener('click',()=> {
+    if(PARAM.style.transform == 'translateX(25vw)'){
+        PARAM.style.transform = 'translateX(0vw)'
+    }
+    else{
+        PARAM.style.transform = 'translateX(25vw)'
+    }
+})
